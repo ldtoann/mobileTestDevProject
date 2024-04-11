cce74d0edb789ea4d67f325a7f476589c64279b9
toan111 <toanlee1311@gmail.com>
2024-04-11 10:58:37 +0700

    mobileTestDevProject_vs2

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-## mobileTestDevProject
+# mobileTestDevProject
 
-## Desription
+# Desription
 
 This project is a React Native application for testing mobile application building.
 
-### Step 1: Install
+## Step 1: Install
 
 First, you will need to clone this repository: "https://github.com/ldtoann/mobileTestDevProject"
 
@@ -14,21 +14,21 @@ Open new terminal from root of project
 
 Use "npm install" to download the necessary libraries in the "node_module" directory
 
-### Step 2: Start Application
+## Step 2: Start Application
 
 To make sure the application runs stably as I said above, you should use android
 
 Open your android emulator
 
-## For Android
+# For Android
 
-## using npm
+# using npm
 
 "npm start"
 
 then press a - run on Android
 
-### Step 3: Login
+## Step 3: Login
 
 At the login screen, you can only log in by mail, I will develop the google login screen soon.
 
--- components/listPR1/index.js
+++ components/listPR1/index.js
@@ -2,84 +2,10 @@
 import React, {useState} from 'react';
 import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
 import styles from './style';
-import {DataList} from '../../data/data';
+import {productData} from '../../data/data';
 
 const Index = () => {
   const borderColors = ['#56698F', '#4ABF40', '#C7577C', '#EDBD57'];
-  const productData = [
-    {
-      id: '01',
-      title: 'Bientôt disponible',
-      name: 'Manette Playstion 4',
-      price: '600 €',
-      price2: '= 6000',
-      cois: '50',
-      user: 'Jacques la malice',
-      location: 'Paris',
-      status: 'Oui',
-      timeToday: '19/03/24',
-      bid: '156',
-      sell: '2 540',
-      coisplus: '+ 15',
-      avatar: require('../../src/image/avatar.png'),
-      image: require('../../src/image/product_1.png'),
-      image2: require('../../src/image/product_h1.png'),
-    },
-    {
-      id: '02',
-      title: 'Bientôt disponible',
-      name: 'Nintendo Switch',
-      price: '130 €',
-      price2: '= 13 000',
-      cois: '100',
-      user: 'Jacques la malice',
-      location: 'Paris',
-      status: 'Non',
-      timeToday: '19/03/24',
-      bid: '123',
-      sell: '9 300',
-      coisplus: '0',
-      avatar: require('../../src/image/avatar.png'),
-      image: require('../../src/image/product_2.png'),
-      image2: require('../../src/image/product_h2.png'),
-    },
-    {
-      id: '03',
-      title: 'Bientôt disponible',
-      name: 'Sac à main Gucci',
-      price: '250 €',
-      price2: '= 25 000',
-      cois: '200',
-      user: 'Jacques la malice',
-      location: 'Paris',
-      status: 'Oui',
-      timeToday: '19/03/24',
-      bid: '365',
-      sell: '11 300',
-      coisplus: '+ 10',
-      avatar: require('../../src/image/avatar.png'),
-      image: require('../../src/image/product_3.png'),
-      image2: require('../../src/image/product_h3.png'),
-    },
-    {
-      id: '04',
-      title: 'Bientôt disponible',
-      name: 'Playstation 4',
-      price: '490 €',
-      price2: '= 49 000',
-      cois: '500',
-      user: 'Jacques la malice',
-      location: 'Paris',
-      status: 'Oui',
-      timeToday: '19/03/24',
-      bid: '893',
-      sell: '23 670',
-      coisplus: '+ 20',
-      avatar: require('../../src/image/avatar.png'),
-      image: require('../../src/image/product_4.png'),
-      image2: require('../../src/image/product_h4.png'),
-    },
-  ];
   const renderItem = ({item, index}) => (
     <View>
       <View
